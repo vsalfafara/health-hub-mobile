446eea79e1cf4c23321eea75b13244fc8741fb7d
vsalfafara <alfafara.vm@gmail.com>
2024-10-22 01:18:44 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'health-hub',
+  webDir: 'dist'
+};
+
+export default config;
